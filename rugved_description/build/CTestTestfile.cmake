53a279bc80ba69dcbe1de80c88284709a1a74380
ishaanagarwal22 <ishaanagarwal2231@gmail.com>
2020-07-27 18:55:04 +0530

    Adding the files

---
+++ rugved_description/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ishaan/catkin_ws/src/rugved_description
+# Build directory: /home/ishaan/catkin_ws/src/rugved_description/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ rugved_description/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ishaan/catkin_ws/src/rugved_description/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ishaan/catkin_ws/src/rugved_description/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/melodic/bin:/home/ishaan/.local/bin:/home/ishaan/bin:/home/ishaan/.nvm/versions/node/v11.0.0/bin:/home/ishaan/.local/bin:/home/ishaan/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/home/ishaan/catkin_ws/src/rugved_description/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/ishaan/catkin_ws/src/rugved_description/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ishaan/catkin_ws/src/rugved_description/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ishaan/catkin_ws/src/rugved_description:$ROS_PACKAGE_PATH"
